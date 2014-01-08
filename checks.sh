00d1a92b2c3ec73fb465ff31e5d7ad03db022c5b
Modif directe du code <dgeo@centrale-marseille.fr>
2014-01-08 11:53:22 +0000

    script pour verifier les points de montages etc.

---
+++ checks.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#
+# verifications diverses des sauvegardes
+#
+. rsync_serveurs.conf
+
+# supprime une propriete (et la sauvegarde en orig:)
+resetprop() {
+  echo "RESETTING $prop on $vol (was \"$val\")" >&2
+  echo zfs inherit $prop $vol
+  echo zfs set orig:${prop}="${val}" $vol
+} 
+
+SAVDESTBASEREGEX="$(echo $SAVDESTBASE | sed 's@/@\\/@g; s/\./\\./g;')"
+zfs get -H -oname,property,value,source -r all $SAVZFSBASE | grep -v '^'$SAVZFSBASE'$' | grep local$ | while read vol prop val src; do
+  case $prop in
+    mountpoint)
+      if [ "$val" = "/" ]; then
+        resetprop
+      else
+        expr "$val" : "$SAVDESTBASEREGEX" >/dev/null || resetprop
+      fi
+    ;;
+    sharenfs)
+      resetprop
+    ;;
+    sharesmb)
+      resetprop
+    ;;
+    dedup)
+      resetprop
+    ;;
+    primarycache)
+      resetprop
+    ;;
+  esac
+done
