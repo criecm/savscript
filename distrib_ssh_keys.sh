94e565895e50ba6fc07612b2baac874ff367a04b
Modif directe du code <dgeo@centrale-marseille.fr>
2014-01-19 20:47:40 +0000

    Utilise la destination depuis la config

--- distrib_ssh_keys.sh
+++ distrib_ssh_keys.sh
@@ -25,12 +25,14 @@ else
 fi
 
 for m in $machines; do
-  if ! ssh -oPasswordAuthentication=no -oStrictHostKeyChecking=yes -oIdentitiesOnly=yes -oIdentityFile=$SSH_KEY -oKbdInteractiveDevices=none root@$m "echo connexion ssh ok" || [ -n "$FORCE" ]; then
-    ssh-keygen -R $m
-    IP=$(host -t a $m | fgrep 'has address' | awk '{print $NF}')
+  eval $(grep ^DEST machines.d/$m.conf)
+  [ -z "$DEST" ] && DEST=$m
+  if ! ssh -oPasswordAuthentication=no -oStrictHostKeyChecking=yes -oIdentitiesOnly=yes -oIdentityFile=$SSH_KEY -oKbdInteractiveDevices=none root@$DEST "echo connexion ssh ok" || [ -n "$FORCE" ]; then
+    ssh-keygen -R $DEST
+    IP=$(host -t a $DEST | fgrep 'has address' | awk '{print $NF}')
     ssh-keygen -R $IP
-    echo "pousse la cle sur ${m}:"
-    echo "from=\""$(ssh -o "StrictHostKeyChecking=no" $m "echo \$SSH_CONNECTION"| awk '{print $1}')"\"$prepend $(cat $SSH_KEY.pub)" > /tmp/k
-    cat /tmp/k | ssh root@$m "cat >> .ssh/tmpkey; ( fgrep -v \"$(cut -d' ' -f2 $SSH_KEY.pub)\" .ssh/authorized_keys ${OLD_KEY:+| fgrep -v \"$(cut -d' ' -f2 $SSH_KEY.pub)\"} ; cat .ssh/tmpkey ) > .ssh/authorized_keys.new && mv .ssh/authorized_keys .ssh/authorized_keys.bak && mv .ssh/authorized_keys.new .ssh/authorized_keys && rm -f .ssh/tmpkey"
+    echo "pousse la cle sur ${DEST}:"
+    echo "from=\""$(ssh -o "StrictHostKeyChecking=no" $DEST "echo \$SSH_CONNECTION"| awk '{print $1}')"\"$prepend $(cat $SSH_KEY.pub)" > /tmp/k
+    cat /tmp/k | ssh root@$DEST "cat >> .ssh/tmpkey; ( fgrep -v \"$(cut -d' ' -f2 $SSH_KEY.pub)\" .ssh/authorized_keys ${OLD_KEY:+| fgrep -v \"$(cut -d' ' -f2 $SSH_KEY.pub)\"} ; cat .ssh/tmpkey ) > .ssh/authorized_keys.new && mv .ssh/authorized_keys .ssh/authorized_keys.bak && mv .ssh/authorized_keys.new .ssh/authorized_keys && rm -f .ssh/tmpkey"
   fi
 done
