46e2a3d916d0fc449b4362538bd32c8a5aed7ac0
Geoffroy Desvernay <dgeo@centrale-marseille.fr>
2017-05-10 01:17:57 +0200

    gestion des "known_hosts" ssh

--- tools/distrib_ssh_keys.sh
+++ tools/distrib_ssh_keys.sh
@@ -28,12 +28,22 @@ fi
 for m in $machines; do
   eval $(grep ^DEST $savpath/machines.d/$m.conf)
   [ -z "$DEST" ] && DEST=$m
-  if ! ssh -oPasswordAuthentication=no -oStrictHostKeyChecking=yes -oIdentitiesOnly=yes -oIdentityFile=$SSH_KEY -oKbdInteractiveDevices=none root@$DEST "echo connexion ssh ok" || [ -n "$FORCE" ]; then
+  echo -n "$m($DEST): "
+  if ! ssh -oConnectTimeout=2 -oPasswordAuthentication=no -oStrictHostKeyChecking=yes -oIdentitiesOnly=yes -oIdentityFile=$SSH_KEY -oKbdInteractiveDevices=none root@$DEST "echo is ok." || [ -n "$FORCE" ]; then
+    MYIP4=$(ssh -4 -oConnectTimeout=2 -oPasswordAuthentication=no -oStrictHostKeyChecking=yes -oIdentitiesOnly=yes -oIdentityFile=$SSH_KEY -oKbdInteractiveDevices=none root@$DEST "echo \$SSH_CLIENT | awk '{print \$1}'")
+    MYIP6=$(ssh -6 -oConnectTimeout=2 -oPasswordAuthentication=no -oStrictHostKeyChecking=yes -oIdentitiesOnly=yes -oIdentityFile=$SSH_KEY -oKbdInteractiveDevices=none root@$DEST "echo \$SSH_CLIENT | awk '{print \$1}'")
     ssh-keygen -R $DEST
+    if [ -z "$MYIP6" ]; then
+      MYIP=$MYIP4
+    elif [ -z "$MYIP4" ]; then
+      MYIP=$MYIP6
+    else
+      MYIP="$MYIP4,$MYIP6"
+    fi
     IP=$(host -t a $DEST | fgrep 'has address' | awk '{print $NF}')
     ssh-keygen -R $IP
-    echo "pousse la cle sur ${DEST}:"
-    echo "from=\""$(ssh -o "StrictHostKeyChecking=no" $DEST "echo \$SSH_CONNECTION"| awk '{print $1}')"\"$prepend $(cat $SSH_KEY.pub)" > /tmp/k
+    echo "from=\"$MYIP\"$prepend $(cat $SSH_KEY.pub)" > /tmp/k
     cat /tmp/k | ssh root@$DEST "cat >> .ssh/tmpkey; ( fgrep -v \"$(cut -d' ' -f2 $SSH_KEY.pub)\" .ssh/authorized_keys ${OLD_KEY:+| fgrep -v \"$(cut -d' ' -f2 $SSH_KEY.pub)\"} ; cat .ssh/tmpkey ) > .ssh/authorized_keys.new && mv .ssh/authorized_keys .ssh/authorized_keys.bak && mv .ssh/authorized_keys.new .ssh/authorized_keys && rm -f .ssh/tmpkey"
+    ssh -oConnectTimeout=2 -oPasswordAuthentication=no -oStrictHostKeyChecking=yes -oIdentitiesOnly=yes -oIdentityFile=$SSH_KEY -oKbdInteractiveDevices=none root@$DEST "echo is now ok."
   fi
 done
