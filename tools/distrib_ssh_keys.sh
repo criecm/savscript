a26834c05e7bc8640e25db572e1a20ca1c76cb6c
Geoffroy Desvernay <dgeo@centrale-marseille.fr>
2017-05-11 13:50:06 +0200

    meilleure detection de la / des IP's

--- tools/distrib_ssh_keys.sh
+++ tools/distrib_ssh_keys.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
 # envoyer la cle ssh dans authorized_keys de la machine
+# (nécéssite une cle deja autorisee et chargée dans ssh-agent)
 #
 # si $OLD_KEY est definie, la supprime des authorized_keys
 # si $FORCE est defini, re-ecrit les authorized_keys
@@ -30,8 +31,8 @@ for m in $machines; do
   [ -z "$DEST" ] && DEST=$m
   echo -n "$m($DEST): "
   if ! ssh -oConnectTimeout=2 -oPasswordAuthentication=no -oStrictHostKeyChecking=yes -oIdentitiesOnly=yes -oIdentityFile=$SSH_KEY -oKbdInteractiveDevices=none root@$DEST "echo is ok." || [ -n "$FORCE" ]; then
-    MYIP4=$(ssh -4 -oConnectTimeout=2 -oPasswordAuthentication=no -oStrictHostKeyChecking=yes -oIdentitiesOnly=yes -oIdentityFile=$SSH_KEY -oKbdInteractiveDevices=none root@$DEST "echo \$SSH_CLIENT | awk '{print \$1}'")
-    MYIP6=$(ssh -6 -oConnectTimeout=2 -oPasswordAuthentication=no -oStrictHostKeyChecking=yes -oIdentitiesOnly=yes -oIdentityFile=$SSH_KEY -oKbdInteractiveDevices=none root@$DEST "echo \$SSH_CLIENT | awk '{print \$1}'")
+    MYIP4=$(ssh -4 -oConnectTimeout=2 -oPasswordAuthentication=no -oStrictHostKeyChecking=no -oKbdInteractiveDevices=none root@$DEST "echo \$SSH_CLIENT | awk '{print \$1}'")
+    MYIP6=$(ssh -6 -oConnectTimeout=2 -oPasswordAuthentication=no -oStrictHostKeyChecking=no -oKbdInteractiveDevices=none root@$DEST "echo \$SSH_CLIENT | awk '{print \$1}'")
     ssh-keygen -R $DEST
     if [ -z "$MYIP6" ]; then
       MYIP=$MYIP4
@@ -41,9 +42,13 @@ for m in $machines; do
       MYIP="$MYIP4,$MYIP6"
     fi
     IP=$(host -t a $DEST | fgrep 'has address' | awk '{print $NF}')
+    IP6=$(host -t a $DEST | fgrep -i 'has IPv6 address' | awk '{print $NF}')
+    echo "from=\"$MYIP\""
     ssh-keygen -R $IP
+    ssh-keygen -R $IP6
+    ssh-keygen -R $DEST
     echo "from=\"$MYIP\"$prepend $(cat $SSH_KEY.pub)" > /tmp/k
-    cat /tmp/k | ssh root@$DEST "cat >> .ssh/tmpkey; ( fgrep -v \"$(cut -d' ' -f2 $SSH_KEY.pub)\" .ssh/authorized_keys ${OLD_KEY:+| fgrep -v \"$(cut -d' ' -f2 $SSH_KEY.pub)\"} ; cat .ssh/tmpkey ) > .ssh/authorized_keys.new && mv .ssh/authorized_keys .ssh/authorized_keys.bak && mv .ssh/authorized_keys.new .ssh/authorized_keys && rm -f .ssh/tmpkey"
+    cat /tmp/k | ssh -oStrictHostKeyChecking=no root@$DEST "cat >> .ssh/tmpkey; ( fgrep -v \"$(cut -d' ' -f2 $SSH_KEY.pub)\" .ssh/authorized_keys ${OLD_KEY:+| fgrep -v \"$(cut -d' ' -f2 $SSH_KEY.pub)\"} ; cat .ssh/tmpkey ) > .ssh/authorized_keys.new && mv .ssh/authorized_keys .ssh/authorized_keys.bak && mv .ssh/authorized_keys.new .ssh/authorized_keys && rm -f .ssh/tmpkey"
     ssh -oConnectTimeout=2 -oPasswordAuthentication=no -oStrictHostKeyChecking=yes -oIdentitiesOnly=yes -oIdentityFile=$SSH_KEY -oKbdInteractiveDevices=none root@$DEST "echo is now ok."
   fi
 done
