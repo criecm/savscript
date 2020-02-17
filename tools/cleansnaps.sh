ebc8663dd3b84c6e1f31c4368c1d205525628dfc
Geoffroy <dgeo@dgeos.net>
2020-02-17 21:59:21 +0100

    zfs_clean_snap in PATH

--- tools/cleansnaps.sh
+++ tools/cleansnaps.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+export PATH=$PATH:/root/tools/scripts/zfs:/usr/local/admin/sysutils/zfs
+savzfs=$(zfs list -H -o name /sav)
+[ -n "$savzfs" ] || exit 1
 for m in $(ls machines.d | grep conf$ | sed s/.conf//); do
-  /usr/local/admin/sysutils/zfs/zfs_clean_snap -r 72h15d6w12m1y zdata/sav/$m
+  zfs_clean_snap -r 72h15d6w12m1y $savzfs/$m
 done
 
