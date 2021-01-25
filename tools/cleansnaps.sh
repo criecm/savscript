d5d3ab787d43e5ea12051d2860e8a76a92f17ffb
geoffroy desvernay <dgeo@centrale-marseille.fr>
2021-01-25 22:39:28 +0100

    log

--- tools/cleansnaps.sh
+++ tools/cleansnaps.sh
@@ -8,5 +8,5 @@ date >> /var/log/cleansnaps.log
 for m in $(ls $savpath/machines.d | grep conf$ | sed 's/\.conf//'); do
   /usr/local/admin/sysutils/zfs/zfs_clean_snap -r 72h15d6w12m1y $SAVZFSBASE/$m | tee -a /var/log/cleansnaps.log
   zfs list -H -o name -r -t snapshot $SAVZFSBASE/$m | fgrep '@'$m'-' | fgrep -v '@'$m'-'$(hostname -s) | tee -a /var/log/cleansnaps.log | xargs -L1 zfs destroy -d
-done
+done >> /var/log/cleansnaps.log
 
