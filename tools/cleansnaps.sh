635535bce3608de8e0ec3669b7e4c4049615a4b0
Geoffroy Desvernay <dgeo@centrale-marseille.fr>
2022-04-14 11:45:59 +0200

    rewrite cleansnap

--- tools/cleansnaps.sh
+++ tools/cleansnaps.sh
@@ -5,8 +5,10 @@ savpath=$(realpath "$(dirname $0)/..")
 . $savpath/savscript.conf || exit 1
 
 date >> /var/log/cleansnaps.log
-for m in $(ls $savpath/machines.d | grep conf$ | sed 's/\.conf//'); do
-  /usr/local/admin/sysutils/zfs/zfs_clean_snap -r 72h15d6w12m1y $SAVZFSBASE/$m | tee -a /var/log/cleansnaps.log
-  zfs list -H -o name -r -t snapshot $SAVZFSBASE/$m | fgrep '@'$m'-' | fgrep -v '@'$m'-'$(hostname -s) | tee -a /var/log/cleansnaps.log | xargs -L1 zfs destroy -d
+for m in $(ls $savpath/machines.d | grep conf$); do
+  . $savpath/machines.d/$m
+  if ! [ -n "$NAME" ]; then echo "$savpath/machines.d/$m: pas de NAME !!!"; continue; fi
+  /usr/local/admin/sysutils/zfs/zfs_clean_snap -r 72h15d6w12m1y $SAVZFSBASE/$NAME | tee -a /var/log/cleansnaps.log
+  zfs list -H -o name -r -t snapshot $SAVZFSBASE/$NAME | fgrep '@'$NAME'-' | fgrep -v '@'$NAME'-'$(hostname -s) | tee -a /var/log/cleansnaps.log | xargs -L1 zfs destroy -d
 done >> /var/log/cleansnaps.log
 
