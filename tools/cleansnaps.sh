0f6aa68d89a8d21586f52a0ffeb2ef47aad07a12
Geoffroy Desvernay <dgeo@centrale-med.fr>
2024-07-26 13:34:51 +0200

    simple log mais simple

--- tools/cleansnaps.sh
+++ tools/cleansnaps.sh
@@ -8,7 +8,6 @@ date >> /var/log/cleansnaps.log
 for m in $(ls $savpath/machines.d | grep conf$); do
   . $savpath/machines.d/$m
   if ! [ -n "$NAME" ]; then echo "$savpath/machines.d/$m: pas de NAME !!!"; continue; fi
-  /usr/local/admin/sysutils/zfs/zfs_clean_snap -r 72h15d6w12m1y $SAVZFSBASE/$NAME | tee -a /var/log/cleansnaps.log
-  zfs list -H -o name -r -t snapshot $SAVZFSBASE/$NAME | fgrep '@'$NAME'-' | fgrep -v '@'$NAME'-'$(hostname -s) | tee -a /var/log/cleansnaps.log | xargs -tL1 zfs destroy -d
-done
-
+  /usr/local/admin/sysutils/zfs/zfs_clean_snap -r 72h15d6w12m1y $SAVZFSBASE/$NAME
+  zfs list -H -o name -r -t snapshot $SAVZFSBASE/$NAME | fgrep '@'$NAME'-' | fgrep -v '@'$NAME'-'$(hostname -s) | xargs -tL1 zfs destroy -d
+done >> /var/log/cleansnaps.log
