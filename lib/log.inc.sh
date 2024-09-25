9495da050751463453870fac3a439aa1445f04a0
geoffroy desvernay <dgeo@centrale-med.fr>
2024-09-25 13:01:35 +0200

    add date to log

--- lib/log.inc.sh
+++ lib/log.inc.sh
@@ -4,6 +4,6 @@ syslogue() {
   prio=${1:-notice}
   shift
   logger -p $SYSLOG_FACILITY.$prio -t"$SYSLOG_TAG[$$]" $@
-  [ $DEBUG -gt 0 -o "$prio" = "error" -o "$prio" = "crit" ] && echo "${prio}: $*" >&2
+  [ $DEBUG -gt 0 -o "$prio" = "error" -o "$prio" = "crit" ] && echo "$(date) [${prio}]: $*" >&2
 }
 
